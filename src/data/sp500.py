a14be2580c8061a8ddf675aad762aa155e8960a8
Бойко Боев <boykopetevboev@gmail.com>
2024-02-06 23:44:30 +0200

    DR: Download sp500 data

---
+++ src/data/sp500.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import pandas as pd
+
+SP500_TICKER = "^GSPC"
+FILE_NAME = "sp500.csv"
+
+def download_SP500_data(start_date, end_date):
+    sp500_data = yf.download(SP500_TICKER, start=start_date, end=end_date)
+    sp500_data.to_csv(FILE_NAME)  
+    
+def get_SP500_data(): 
+    sp500_data = pd.read_csv(FILE_NAME)
+
+    sp500_data['Date'] = pd.to_datetime(sp500_data['Date'])
+    sp500_data.set_index('Date', inplace=True)
+    return sp500_data
