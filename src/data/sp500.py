cb491ccae783ede13cc6be6f535fb10b1ddb1a9a
Boyko Boev <boyko.boev@mentormate.com>
2024-02-07 20:39:17 +0200

    DR: sp500 data update

--- src/data/sp500.py
+++ src/data/sp500.py
@@ -8,9 +8,13 @@ def download_SP500_data(start_date, end_date):
     sp500_data = yf.download(SP500_TICKER, start=start_date, end=end_date)
     sp500_data.to_csv(FILE_NAME)  
     
-def get_SP500_data(): 
+def get_SP500_data(start_date, end_date): 
     sp500_data = pd.read_csv(FILE_NAME)
 
     sp500_data['Date'] = pd.to_datetime(sp500_data['Date'])
+    
+    sp500_data = sp500_data[(sp500_data['Date'] >= start_date) & (sp500_data['Date'] <= end_date)]
+
     sp500_data.set_index('Date', inplace=True)
+
     return sp500_data
