0f249b0ea13d7c7e2aeb01dfdd1c9e435253fcb4
Boyko Boev <boyko.boev@mentormate.com>
2024-02-25 21:44:11 +0200

    DR: Download dow jones industrial average data

---
+++ src/data/dow_jones/dow_jones.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+from constants import DJI_TICKER
+
+FILE_NAME = "./data/dow_jones/dow_jones.csv"
+
+def download_dow_jones_data():
+    dow_jones_data = yf.download(DJI_TICKER, interval='1d')
+    dow_jones_data.to_csv(FILE_NAME)  
+    
+def get_dow_jones_data(start_date, end_date): 
+    dow_jones_data = pd.read_csv(FILE_NAME)
+    dow_jones_data = dow_jones_data[(dow_jones_data['Date'] >= start_date) & (dow_jones_data['Date'] <= end_date)]
+    return dow_jones_data
